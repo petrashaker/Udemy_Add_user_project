e675d4aa8a08b79c7dac439d44eaad98a0b564bb
petrashaker <petra.shaker@gmail.com>
2022-06-12 23:36:00 +0200

    Using createPortal() for Modal render

--- src/components/UI/ErrorModal/ErrorModal.jsx
+++ src/components/UI/ErrorModal/ErrorModal.jsx
@@ -1,28 +1,35 @@
-import React, { useState } from "react";
+import React from "react";
+import ReactDom from "react-dom";
 import Button from "../Button/Button";
 import Card from "../Card/Card";
 import styles from "./ErrorModal.module.css";
 
-const ErrorModal = ({title, message, onClick}) => {
-    const [close, setClose] = useState(false);
-    const closeModal = () => {
-        setClose(true);
-    }
+const Backdrop = ({onClick}) => {
+    return <div className={styles.backdrop} onClick={onClick} />
+}
 
+const ModalOverlay = ({title, message, onClick}) => {
+    return (
+        <Card className={styles.modal}>
+        <header className={styles.header}>
+            <h2>{title}</h2>
+        </header>
+        <div className={styles.content}>
+            <p>{message}</p>
+        </div>
+        <footer className={styles.actions}>
+            <Button onClick={onClick}>OK</Button>
+        </footer>
+    </Card>
+
+    )
+}
+
+const ErrorModal = ({title, message, onClick}) => {
     return (
         <>
-        <div className={styles.backdrop} onClick={onClick} />
-        <Card className={styles.modal}>
-            <header className={styles.header}>
-                <h2>{title}</h2>
-            </header>
-            <div className={styles.content}>
-                <p>{message}</p>
-            </div>
-            <footer className={styles.actions}>
-                <Button onClick={onClick}>OK</Button>
-            </footer>
-        </Card>
+        {ReactDom.createPortal(<Backdrop onClick={onClick} />, document.getElementById("backdrop-root"))}
+        {ReactDom.createPortal(<ModalOverlay title={title} message={message} onClick={onClick} />, document.getElementById("modal-root"))}
         </>
     )
 };
